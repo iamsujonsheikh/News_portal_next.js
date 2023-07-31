27c1dea1359b179b65a68a56ee754dbbebf209c6
sujonphero <sujonsheikh.in@gmail.com>
2023-07-31 11:45:42 +0600

    done single page

--- src/pages/news/[newsId].js
+++ src/pages/news/[newsId].js
@@ -1,10 +1,81 @@
 import RootLayout from '@/components/Layouts/RootLayout';
+import { Col, Row } from 'antd';
+import Image from 'next/image';
 import React from 'react'
+import {
+    ArrowRightOutlined,
+    CalendarOutlined,
+    CommentOutlined,
+    ProfileOutlined,
+} from "@ant-design/icons";
 
 const NewsIdDetails = ({ news }) => {
+    if (!news) {
+        return <p style={{ color: "red" }}>LOADING................</p>
+    }
     return (
-        <div>
-            <h1>Title: {news.title}</h1>
+        <div style={{ marginTop: "50px" }}>
+            <Row
+                gutter={{
+                    xs: 8,
+                    sm: 16,
+                    md: 24,
+                    lg: 32,
+                }}
+            >
+                <Col className="gutter-row" span={12}>
+                    <div>
+                        <Image
+                            src={news?.image_url}
+                            width={500}
+                            height={300}
+                            responsive
+                            alt="news image"
+                        />
+                    </div>
+                </Col>
+                <Col className="gutter-row" span={12}>
+                    <div>
+                        <h1 style={{ fontSize: "25px" }}>{news?.title}</h1>
+
+                        <p style={{ color: "blue" }}>Author : {news?.author}</p>
+                        <div
+                            className="line"
+                            style={{
+                                height: "5px",
+                                margin: "20px 0",
+                                background: "#000",
+                                width: "100%",
+                            }}
+                        ></div>
+
+                        <p
+                            style={{
+                                display: "flex",
+                                justifyContent: "space-between",
+                                width: "100%",
+                                color: "gray",
+                                margin: "10px 0px",
+                                fontSize: "12px",
+                            }}
+                        >
+                            <span>
+                                <CalendarOutlined /> {news?.release_date}
+                            </span>
+                            <span>
+                                <CommentOutlined /> {news?.comment_count} COMMENTS
+                            </span>
+                            <span>
+                                <ProfileOutlined /> {news?.category}
+                            </span>
+                        </p>
+
+                        <p style={{ fontSize: "20px" }}>
+                            {news?.description}
+                        </p>
+                    </div>
+                </Col>
+            </Row>
         </div>
     )
 }
